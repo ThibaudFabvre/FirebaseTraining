fa80db9bb4fb7714bb8010855532b9a0850e12f3
ThibaudFabvre <thibaudfabvre@gmail.com>
2020-01-08 16:58:31 +0100

    add missing individual keys for each Task

--- views/List.tsx
+++ views/List.tsx
@@ -13,7 +13,7 @@ const List: React.FC<Props> = ({ list, color, deleteTaskFromList, reloadTasksLis
     return (
         <View style={{backgroundColor: color, justifyContent: 'space-around', alignItems: 'center'}}>
             {list.map(
-                ({title, resume, id}) => <Task reloadTasksList={reloadTasksList} deleteTask= {deleteTaskFromList} id={id} title={title} resume={resume}/>
+                ({title, resume, id}) => <Task key={id} reloadTasksList={reloadTasksList} deleteTask= {deleteTaskFromList} id={id} title={title} resume={resume}/>
             )}
         </View>
     );
